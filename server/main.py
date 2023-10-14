386698f6f5c2c8d05ec09ab196840928587bc308
Aaron Tran <dahpaladin123@gmail.com>
2023-10-14 17:16:11 -0400

    palmapi push

--- server/main.py
+++ server/main.py
@@ -1,6 +1,28 @@
 def main():
-    print("wassp")
-    print('hi')
+    import pprint
+    import google.generativeai as palm
+
+    API_KEY = 'AIzaSyB15U8lI0rvgNr0mLDZO287OcuF_GJL5tU'
+
+    palm.configure(api_key=API_KEY)
+
+    models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+    model = models[0].name
+    print(model)
+
+    prompt = """
+    Imagine you're a verizon employee who's only knowledgeable on phones, service plans, repairing devices, and things closely related to networking and phones and you're answering a customer. You cannot answer questions or provide not having that do not relate to your job. Heres the question: What stroage options does the galaxy s23+ come with and say the prices of each?
+    """
+ #test comment
+    completion = palm.generate_text(
+        model=model,
+        prompt=prompt,
+        temperature=0,
+        # The maximum length of the response
+        max_output_tokens=800,
+    )
+
+    print(completion.result)
 
 
 
