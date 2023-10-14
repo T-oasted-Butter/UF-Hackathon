0d3881d0480f9b18bf169e080c2fca3ff40d4e73
michaelkalai <michael.kalai124@gmail.com>
2023-10-14 18:26:40 -0400

    added comments

--- server/server.py
+++ server/server.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
 app = Flask(__name__)
  
- #sends "J"
+ #sends "Hello World on server startup"
 @app.route('/', methods=['GET'])
 def hello():
    return 'Hello World'
 
+#tests server's ability to receive files
 @app.route('/posttest', methods=['POST'])
 def posttest():
    data = request.get_json()
