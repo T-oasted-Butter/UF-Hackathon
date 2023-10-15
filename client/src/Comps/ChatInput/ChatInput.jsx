93591ad98e0101ec99006e9ca6f22f904ffc1235
Khan <omarkhan7@deloitte.com>
2023-10-15 10:06:23 -0400

    added bare bones ui

---
+++ client/src/Comps/ChatInput/ChatInput.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import './ChatInput.css';
+
+const ChatInput = ({ onMessageSubmit }) => {
+  const [message, setMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (message.trim() !== '') {
+      onMessageSubmit(message);
+      setMessage('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="chat-input">
+      <input
+        type="text"
+        placeholder="Type your message..."
+        value={message}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+export default ChatInput;
