c9d2a05cd9a3fac5cb9c18f423da58ee0c5bcfe8
Aaron Tran <dahpaladin123@gmail.com>
2023-10-14 20:43:01 -0400

    palm works

--- server/main.py
+++ server/main.py
@@ -5,26 +5,27 @@ from dotenv import load_dotenv
 load_dotenv()
 def main():
     
-
     PALMSECRET_KEY = os.getenv('PALM_KEY')
 
     palm.configure(api_key=PALMSECRET_KEY)
 
+    #checks the available palm api models
     models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
     model = models[0].name
     print(model)
 
-    prompt = """
+    prompt = '''
 Imagine you're a verizon employee who's only knowledgeable on phones, service plans, 
 repairing devices, and things closely related to networking and phones and you're answering a customer with a question. Heres the question: How do you harvest corn?
 
-    """
+    '''
  #test comment
     completion = palm.generate_text(
         model=model,
         prompt=prompt,
+        #controls the randomness of the response
         temperature=0,
-        # The maximum length of the response
+        # The maximum length of the response, 1200 maximum
         max_output_tokens=800,
     )
 
