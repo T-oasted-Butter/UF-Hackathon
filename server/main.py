6bb7ab6d53604ea72f7f185fb849474e49dc6654
Aaron Tran <dahpaladin123@gmail.com>
2023-10-15 09:37:00 -0400

    addded double quotes

--- server/main.py
+++ server/main.py
@@ -2,29 +2,35 @@ import pprint
 import google.generativeai as palm
 import os
 from dotenv import load_dotenv
+from datetime import date
 load_dotenv()
 def main():
+
+
+    today = date.today()
     
-    PALMSECRET_KEY = os.getenv('PALM_KEY')
+    
+    PALMSECRET_KEY = os.getenv("PALM_KEY")
 
     palm.configure(api_key=PALMSECRET_KEY)
 
     #checks the available palm api models
-    models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+    models = [m for m in palm.list_models() if "generateText" in m.supported_generation_methods]
     model = models[0].name
     print(model)
 
-    prompt = '''
-Imagine you're a verizon employee who's only knowledgeable on phones, service plans, 
-repairing devices, and things closely related to networking and phones and you're answering a customer with a question. Heres the question: How do you harvest corn?
+    prompt = """
+    Imagine you're a Verizon employee who has the most up-to-date information as of {} and accurate information from the internet and who's only knowledgeable on phones, service plans, repairing devices, and things closely related to networking and phones and you're answering a customer.
+    You cannot answer questions or provide information that do not relate to your job. Here's the question: What is the screen size of the Pixel?
+    """.format(today)
 
-    '''
+    
  #test comment
     completion = palm.generate_text(
         model=model,
         prompt=prompt,
         #controls the randomness of the response
-        temperature=0,
+        temperature=.8,
         # The maximum length of the response, 1200 maximum
         max_output_tokens=800,
     )
