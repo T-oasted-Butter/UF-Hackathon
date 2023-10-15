9b2060480f727afea2e5137c7837dcfef9cb872b
Khan <omarkhan7@deloitte.com>
2023-10-15 16:52:10 -0400

    added axio

--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from ask_bot import ask_bot
 app = Flask(__name__)
+CORS(app)
+
  
 # sends user prompt to palmapi and returns response
 @app.route("/askbot", methods=["POST"])
