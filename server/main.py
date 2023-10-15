5a07a854f51934561831b9e3d5cd0427e683963a
Aaron Tran <dahpaladin123@gmail.com>
2023-10-15 13:02:29 -0400

    user_question added

--- server/main.py
+++ server/main.py
@@ -4,14 +4,11 @@ import os
 from dotenv import load_dotenv
 from datetime import date
 load_dotenv()
-def main():
-    #print(os.getcwd())
-    verizon_data = open("./verizon_dataset.txt","r")
-    method = verizon_data.read
-    verizon_data.close
-    a=method()
-    #print(a)
-    today = date.today()
+def ask_bot(user_question):
+
+
+    
+
     
     
     PALMSECRET_KEY = os.getenv("PALM_KEY")
@@ -25,9 +22,10 @@ def main():
 
     prompt = """
     Imagine you're a Verizon employee who has the most up-to-date information who's only knowledgeable on phones, service plans, repairing devices, 
-    and things closely related to networking and phones and you're answering a customer. Here's some extra information about verizon: {}
-    You cannot answer questions or provide information/help that do not specfically relate to your job or to what a verizon employee would do in real life. Here's the question: 10101010?
-    """.format(a)
+    and things closely related to networking and phones and you're answering a customer.
+    You cannot answer questions or provide information/help that do not specfically relate to your job or to what a verizon employee would do in real life. 
+    Here's the question: 
+    """+ user_question
 
     
  #test comment
@@ -45,4 +43,4 @@ def main():
 
 
 if __name__== "__main__":
-    main()
+    ask_bot('what are the google pixel 7a specs?')
