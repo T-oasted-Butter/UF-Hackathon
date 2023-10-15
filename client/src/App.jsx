ae19709da8182e6a0176251f81c31c163c3595a7
Khan <omarkhan7@deloitte.com>
2023-10-15 15:16:23 -0400

    added spacing stuff

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,24 @@ import Header from './Comps/Header/Header';
 function App() {
   const [messages, setMessages] = useState([{ sender: 'shabounig', text: "Hi welcome to verizon, I am your designated chatbot. I am tasked with helping you answer any question you may have." }]);
 
-  const handleNewMessage = (message) => {
-    const newMessage = { sender: 'user', text: message };
+  const handleNewMessage = (message, sender) => {
+    const newMessage = { sender: sender, text: message };
     setMessages([...messages, newMessage]);
   };
 
   return (
     <div className="App">
       <Header/>
+      <br/>
+      <br/>
+      <div class='temp1'>
         <center>
           <div className="chat-container">
             <ChatBox messages={messages} />
             <ChatInput onMessageSubmit={handleNewMessage}/>
           </div>
         </center>
+      </div>
     </div>
   );
 }
