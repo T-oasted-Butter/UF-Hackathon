8556584accff86b8779881f0c5e2a0d59a60e11f
ChefOmarDee <okhan@vazkor.com>
2023-10-20 18:22:06 -0400

    centered textbox

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,40 @@
-import React, { useState } from 'react';
-import ChatBox from './Comps/ChatBox/ChatBox';
-import ChatInput from './Comps/ChatInput/ChatInput';
-import Header from './Comps/Header/Header';
-import LanguageProvider from './Comps/Context/LanguageProvider';
+import React, { useState } from "react";
+import ChatBox from "./Comps/ChatBox/ChatBox";
+import ChatInput from "./Comps/ChatInput/ChatInput";
+import Header from "./Comps/Header/Header";
+import LanguageProvider from "./Comps/Context/LanguageProvider";
 
 function App() {
-  const [messages, setMessages] = useState([{ sender: 'shabounig', text: "Hi welcome to verizon, I am your designated chatbot. I am tasked with helping you answer any question you may have." }]);
+	const [messages, setMessages] = useState([
+		{
+			sender: "shabounig",
+			text: "Hi welcome to verizon, I am your designated chatbot. I am tasked with helping you answer any question you may have.",
+		},
+	]);
 
-  const handleNewMessage = (message, sender) => {
-    setMessages((prevMessages) => {
-      return [...prevMessages, { sender, text: message }];
-    });
-  };
+	const handleNewMessage = (message, sender) => {
+		setMessages((prevMessages) => {
+			return [...prevMessages, { sender, text: message }];
+		});
+	};
 
-  return (
-    <div className="App">
-      <LanguageProvider>
-      <Header/>
-      <br/>
-      <br/>
-      <div className='temp1'>
-        <center>
-          <div className="chat-container">
-            <ChatBox messages={messages} />
-            <ChatInput onMessageSubmit={handleNewMessage}/>
-          </div>
-        </center>
-      </div>
-      </LanguageProvider>
-    </div>
-  );
+	return (
+		<div className="App">
+			<LanguageProvider>
+				<Header />
+				<br />
+				<br />
+				<div className="temp1">
+					<center>
+						<div className="chat-container">
+							<ChatBox messages={messages} />
+							<ChatInput onMessageSubmit={handleNewMessage} />
+						</div>
+					</center>
+				</div>
+			</LanguageProvider>
+		</div>
+	);
 }
 
 export default App;
